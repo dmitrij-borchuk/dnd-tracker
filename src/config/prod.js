4732c3bbe1cf953976e219bfedea9a53b244afa4
Dmytro Borchuk <dborchuk@remit.se>
2019-01-13 17:27:56 +0200

    Test env vars

--- src/config/prod.js
+++ src/config/prod.js
@@ -1,6 +1,9 @@
 // TODO: add env vars
 export default {
-  apiKey: '<Web API Key>',
-  authDomain: '<Project ID>.firebaseapp.com',
-  databaseURL: '<Database url>',
+  // apiKey: 'AIzaSyBPFQ-iZ-LITlvs-c-W1Tq9uTfLZ6TYmtI',
+  // authDomain: 'd-n-d-tracker.firebaseapp.com',
+  // databaseURL: 'https://d-n-d-tracker.firebaseio.com',
+  projectId: process.env.projectId,
+  // storageBucket: 'd-n-d-tracker.appspot.com',
+  // messagingSenderId: '697087971098',
 };
